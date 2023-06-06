97a636894f3ace7331f0a8d918f328735a222276
gemtunde <gemtunde@gmail.com>
2023-06-06 14:37:13 +0100

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const app = require("./app");
+
+//Handling uncaugth error
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for handling uncaught exception`);
+});
+
+//config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
+}
+
+//create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
+
+//unhandle promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
