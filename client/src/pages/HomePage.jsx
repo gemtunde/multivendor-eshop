c01e3ff10db414769bf89172f1013937dce59a4f
gemtunde <gemtunde@gmail.com>
2023-06-14 12:38:14 +0100

    featured products

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,14 +3,18 @@ import Header from "../components/Layout/Header.jsx";
 import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
+import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
+      <Header activeHeading={1} />
       <Hero />
       <Categories />
+      <Categories />
       <BestDeals />
+      <FeaturedProduct />
     </div>
   );
 };
